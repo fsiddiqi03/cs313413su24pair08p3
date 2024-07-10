67ef1b2bc7367abe154b82aa5391d760a3eeaf98
Binod Shah <ps.binod@gmail.com>
2024-07-09 22:31:08 -0500

    Modified the Draw.java file

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/android/Draw.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/android/Draw.java
@@ -17,8 +17,8 @@ public class Draw implements Visitor<Void> {
     private final Paint paint;
 
     public Draw(final Canvas canvas, final Paint paint) {
-        this.canvas = null; // FIXME
-        this.paint = null; // FIXME
+        this.canvas = canvas; // FIXME
+        this.paint = paint; // FIXME
         paint.setStyle(Style.STROKE);
     }
 
@@ -30,44 +30,74 @@ public class Draw implements Visitor<Void> {
 
     @Override
     public Void onStrokeColor(final StrokeColor c) {
-
+        paint.setColor(c.getColor());
+        c.getShape().accept(this);
         return null;
     }
 
     @Override
     public Void onFill(final Fill f) {
 
+        final Style original = paint.getStyle();
+        paint.setStyle(Style.FILL);
+        f.getShape().accept(this);
+        paint.setStyle(original);
         return null;
     }
 
     @Override
     public Void onGroup(final Group g) {
 
+        for (Shape s : g.getShapes()) {
+            s.accept(this);
+        }
         return null;
     }
 
     @Override
     public Void onLocation(final Location l) {
 
+        canvas.save();
+        canvas.translate(l.getX(), l.getY());
+        l.getShape().accept(this);
+        canvas.restore();
         return null;
     }
 
     @Override
     public Void onRectangle(final Rectangle r) {
 
+        canvas.drawRect(0, 0, r.getWidth(), r.getHeight(), paint);
         return null;
     }
 
     @Override
     public Void onOutline(Outline o) {
 
+        final Style original = paint.getStyle();
+        paint.setStyle(Style.STROKE);
+        o.getShape().accept(this);
+        paint.setStyle(original);
         return null;
     }
 
     @Override
     public Void onPolygon(final Polygon s) {
 
-        final float[] pts = null;
+        final int size = p.getPoints().size();
+        if (size < 2) return null;
+
+        final float[] pts = new float[(size + 1) * 4];
+        int index = 0;
+
+        for (int i = 0; i < size; i++) {
+            Point start = p.getPoints().get(i);
+            Point end = p.getPoints().get((i + 1) % size);
+            pts[index++] = start.getX();
+            pts[index++] = start.getY();
+            pts[index++] = end.getX();
+            pts[index++] = end.getY();
+        }
 
         canvas.drawLines(pts, paint);
         return null;
