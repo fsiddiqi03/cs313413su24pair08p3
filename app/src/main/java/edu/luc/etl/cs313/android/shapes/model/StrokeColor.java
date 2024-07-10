8b436c28be61872c0472f780dbb8f782cd37e30f
fsiddiqi03 <farissiddiqi@gmail.com>
2024-07-10 13:46:03 -0500

    Completed the StrokeColor class. - Constructor initializes a shape and color - getColor, returns color - getShape, returns shape - implemented accept method to handle visitors

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/model/StrokeColor.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/model/StrokeColor.java
@@ -7,20 +7,24 @@ package edu.luc.etl.cs313.android.shapes.model;
 public class StrokeColor implements Shape {
 
     // TODO entirely your job
+    protected final int color;
+    protected final Shape shape;
 
     public StrokeColor(final int color, final Shape shape) {
+        this.shape = shape;
+        this.color = color;
     }
 
     public int getColor() {
-        return -1;
+        return color;
     }
 
     public Shape getShape() {
-        return null;
+        return shape;
     }
 
     @Override
     public <Result> Result accept(Visitor<Result> v) {
-        return null;
+        return v.onStrokeColor(this);
     }
 }
